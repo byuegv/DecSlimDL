e35f4fb5a1b4d336a4be45be741fb8d7cc562733
wdaemon <byuegv@163.com>
2020-08-09 21:22:08 +0800

    serialization without utf8

--- src/main/scala/com/intel/analytics/bigdl/optim/SerializationUtil.scala
+++ src/main/scala/com/intel/analytics/bigdl/optim/SerializationUtil.scala
@@ -5,22 +5,24 @@ import java.util.Base64
 import java.nio.charset.StandardCharsets.UTF_8
 
 object SerializationUtil {
-  def serialize(value: Any): String = {
+  def serialize[T](value: T): String = {
     val stream: ByteArrayOutputStream = new ByteArrayOutputStream()
     val oos = new ObjectOutputStream(stream)
     oos.writeObject(value)
-    oos.close
-    new String(
-      Base64.getEncoder().encode(stream.toByteArray),
+    oos.close()
+    /*new String(
+      Base64.getEncoder().encode(stream.toByteArray()),
       UTF_8
-    )
+    )*/
+    Base64.getEncoder().encodeToString(stream.toByteArray())
   }
 
   def deserialize[T](str: String) = {
-    val bytes = Base64.getDecoder().decode(str.getBytes(UTF_8))
+//    val bytes = Base64.getDecoder().decode(str.getBytes(UTF_8))
+    val bytes = Base64.getDecoder().decode(str)
     val ois = new ObjectInputStream(new ByteArrayInputStream(bytes))
-    val value = ois.readObject.asInstanceOf[T]
-    ois.close
+    val value = ois.readObject().asInstanceOf[T]
+    ois.close()
     value
   }
 }
